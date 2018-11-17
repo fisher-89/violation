699ad11f5a237d8e4a3c2feb3907c22f9387143d
354226867 <354226867@qq.com>
2018-11-17 15:13:59 +0800

    first

--- packages/package-sso/src/Traits/ResourceLibrary.php
+++ packages/package-sso/src/Traits/ResourceLibrary.php
@@ -6,7 +6,7 @@ trait ResourceLibrary
 {
     /**
      * 获取员工信息.
-     * 
+     *
      * @param $params 非数组为员工主键
      * @return mixed
      */
@@ -17,12 +17,12 @@ trait ResourceLibrary
             return $this->get('api/staff', $params);
         }
 
-        return $this->get('api/staff/'.$params);
+        return $this->get('api/staff/' . $params);
     }
 
     /**
      * 获取部门信息.
-     * 
+     *
      * @author 28youth
      * @param  $params 非数组为部门主键
      * @return mixed
@@ -30,16 +30,16 @@ trait ResourceLibrary
     public function getDepartmenets($params)
     {
         if (is_array($params)) {
-            
+
             return $this->get('api/departments', $params);
         }
 
-        return $this->get('api/departments/'.$params);
+        return $this->get('api/departments/' . $params);
     }
 
     /**
      * 获取品牌信息.
-     * 
+     *
      * @author 28youth
      * @param  非数组为品牌主键
      * @return mixed
@@ -47,16 +47,16 @@ trait ResourceLibrary
     public function getBrands($params)
     {
         if (is_array($params)) {
-            
+
             return $this->get('api/brands', $params);
         }
 
-        return $this->get('api/brands/'.$params);
+        return $this->get('api/brands/' . $params);
     }
 
     /**
      * 获取位置信息.
-     * 
+     *
      * @author 28youth
      * @param  非数组为主键
      * @return mixed
@@ -64,16 +64,16 @@ trait ResourceLibrary
     public function getPositions($params)
     {
         if (is_array($params)) {
-            
+
             return $this->get('api/positions', $params);
         }
 
-        return $this->get('api/positions/'.$params);
+        return $this->get('api/positions/' . $params);
     }
 
     /**
      * 获取商品信息.
-     * 
+     *
      * @author 28youth
      * @param  非数组为商品主键
      * @return mixed
@@ -81,16 +81,16 @@ trait ResourceLibrary
     public function getShops($params)
     {
         if (is_array($params)) {
-            
+
             return $this->get('api/shops', $params);
         }
 
-        return $this->get('api/shops/'.$params);
+        return $this->get('api/shops/' . $params);
     }
 
     /**
      * 获取用户角色信息.
-     * 
+     *
      * @author 28youth
      * @param  非数组为主键
      * @return mixed
@@ -98,16 +98,16 @@ trait ResourceLibrary
     public function getRoles($params)
     {
         if (is_array($params)) {
-            
+
             return $this->get('api/roles', $params);
         }
 
-        return $this->get('api/roles/'.$params);
+        return $this->get('api/roles/' . $params);
     }
 
     /**
      * 获取钉钉access_token.
-     * 
+     *
      * @author 28youth
      * @return access_token
      */
@@ -122,16 +122,29 @@ trait ResourceLibrary
      */
     public function getPoints($params)
     {
-        return $this->request('get','/admin/point-log');
+        return $this->request('get', '/admin/point-log');
     }
 
     /**
+     * 添加数据
+     *
      * @param $arr
      * @return mixed
      */
     public function postPoints($arr)
     {
-        return $this->request('post','/admin/point-log/',$arr);
+        return $this->request('post', '/admin/point-log/', $arr);
+    }
+
+    /**
+     * 删除数据
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function point($id)
+    {
+        return $this->request('delete', '/admin/point-log/' . $id);
     }
 
     public function getDepartmenetAll($params)
@@ -140,6 +153,6 @@ trait ResourceLibrary
 
             return $this->get('api/department/get_tree', $params);
         }
-        return $this->get('api/department/get_tree/'.$params);
+        return $this->get('api/department/get_tree/' . $params);
     }
 }
