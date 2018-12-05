eca31e967cd32cc265a7f00edcc3e2fe0b479ded
354226867 <354226867@qq.com>
2018-12-05 19:18:00 +0800

    first钉钉推送地址

--- config/sso.php
+++ config/sso.php
@@ -7,4 +7,5 @@ return [
     'client_id' => env('CLIENT_ID', 0),
     'client_secret' => env('CLIENT_SECRET', ''),
     'point_host' => env('POINT_HOST', ''),
+    'ding_host' =>env('DING_HOST',''),
 ];
