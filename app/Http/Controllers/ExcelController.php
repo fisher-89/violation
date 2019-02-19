36950d5cf2ab25872d1ca06db6206e3a347b5c97
354226867 <354226867@qq.com>
2019-02-19 17:16:01 +0800

    first 数据格式整理，改变付款状态字段启用

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\DingGroup;
+use App\Models\PunishHasAuth;
+use App\Models\Pushing;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Services\PunishService;
@@ -21,19 +23,23 @@ class ExcelController extends Controller
     protected $error;
     protected $RulesModel;
     protected $punishModel;
+    protected $pushingModel;
+    protected $punishHasAuth;
     protected $punishService;
     protected $dingGroupModel;
     protected $countStaffModel;
     protected $produceMoneyService;
     protected $countHasPunishModel;
 
-    public function __construct(PunishService $punishService, CountService $countService, Punish $punish, Rules $rules,
-                                CountStaff $countStaff, CountHasPunish $countHasPunish, DingGroup $dingGroup)
+    public function __construct(PunishService $punishService, CountService $countService, Punish $punish, Rules $rules, PunishHasAuth $punishHasAuth,
+                                CountStaff $countStaff, CountHasPunish $countHasPunish, DingGroup $dingGroup, Pushing $pushing)
     {
         $this->RulesModel = $rules;
         $this->punishModel = $punish;
+        $this->pushingModel = $pushing;
         $this->dingGroupModel = $dingGroup;
         $this->countStaffModel = $countStaff;
+        $this->punishHasAuth = $punishHasAuth;
         $this->punishService = $punishService;
         $this->produceMoneyService = $countService;
         $this->countHasPunishModel = $countHasPunish;
@@ -53,7 +59,7 @@ class ExcelController extends Controller
     }
 
     /**
-     * 导入 todo 如果是已付款，改变付款状态人员填写，加字段填写推送群
+     * 导入
      *
      * @param Request $request
      * @return mixed
@@ -98,7 +104,6 @@ class ExcelController extends Controller
             } else {
                 $this->error['开单人编号'][] = '不正确';
             }
-
             $rule = $this->RulesModel->where('name', $res[$i][3])->first();
             $check = $rule != null ? $rule->id : null;
             if ((bool)$check === false) {
@@ -126,10 +131,12 @@ class ExcelController extends Controller
                 'score' => $msg['ruleId'] != null && $msg['staffSn'] != null && $msg['violateAt'] != null ? $this->produceMoneyService->generate($oaData, $msg, 'score') : null,
                 'violate_at' => $res[$i][4],
                 'has_paid' => is_numeric($res[$i][7]) ? (int)$res[$i][7] : $res[$i][7],
+                'action_staff_sn' => $res[$i][7] == 1 ? $request->user()->realname : null,
                 'paid_at' => $res[$i][7] == 1 ? $res[$i][8] == true ? $res[$i][8] : date('Y-m-d H:i:s') : null,
                 'month' => date('Ym'),
                 'remark' => $res[$i][9],
-                'sync_point' => is_numeric($res[$i][10]) ? (int)$res[$i][10] : $res[$i][10],
+                'pushing' => $this->pushingDispose($res[$i][10]),
+                'sync_point' => is_numeric($res[$i][11]) ? (int)$res[$i][11] : $res[$i][11],
                 'creator_sn' => $request->user()->staff_sn,
                 'creator_name' => $request->user()->realname,
             ];
@@ -137,12 +144,20 @@ class ExcelController extends Controller
             $this->excelDataVerify($object);
             if ($this->error == []) {
                 $data = $this->punishService->excelSave($sql);
+                $hasSql = [];
+                foreach ($sql['pushing'] as $punishValue) {
+                    $hasSql[] = [
+                        'punish_id' => $data->id,
+                        'auth_id' => $punishValue
+                    ];
+                }
+                $this->punishHasAuth->insert($hasSql);
                 if (substr($data->billing_at, 0, 7) != date('Y-m')) {
                     $this->punishService->eliminateUltimoBill($data);
                 }
                 $object->brand_name = $oaData['brand']['name'];
                 $this->punishService->updateCountData($object, $data, 1);
-                if ($res[$i][10] == 1) {
+                if ($res[$i][11] == 1) {
                     $point[] = $this->pointSql($rule, $object, $oaData, $data->id);
                 }
                 if ($data == true) {
@@ -177,6 +192,20 @@ class ExcelController extends Controller
         $info['headers'] = isset($header) ? $header : [];
         $info['errors'] = isset($mistake) ? $mistake : [];
         return $info;
+    }
+
+    protected function pushingDispose($string)
+    {
+        $pushingArray = explode(',', $string);
+        $array = [];
+        foreach ($pushingArray as $value) {
+            $id = $this->pushingModel->where(['flock_name' => $value, 'staff_sn' => Auth::user()->staff_sn])->value('id');
+            if ($id == false) {
+                $this->error['推送群'][] = $value . '没找到';
+            }
+            $array[] = $id;
+        }
+        return $array;
     }
 
     public function staffExcel(Request $request)
@@ -253,7 +282,7 @@ class ExcelController extends Controller
         $rule = array_column($assist == null ? [] : $assist->toArray(), 'name');
         $flockName = array_column($pushingObj == null ? [] : $pushingObj->toArray(), 'flock_name');
         $cellData[] = ['员工编号', '员工姓名', '开单日期', '大爱名称', '违纪时间', '开单人编号', '开单人姓名', '是否付款', '付款时间', '备注', '推送的群', '同步积分制'];
-        $cellData[] = ['例：100000（被大爱编号）', '例：张三（被大爱姓名）', '例：2018-01-01（开单时间）', '例：迟到30分钟内（制度名称全写）', '例：2018-01-01', '例：100000（开单人编号）', '例：李四', '例：0（0：表示没有付款，1：表示已经付款）', '例：2018-01-01（没有付款这里为空）', '默认为空', '例：喜歌实业重要通知群','默认不同步，1:同步'];
+        $cellData[] = ['例：100000（被大爱编号）', '例：张三（被大爱姓名）', '例：2018-01-01（开单时间）', '例：迟到30分钟内（制度名称全写）', '例：2018-01-01', '例：100000（开单人编号）', '例：李四', '例：0（0：表示没有付款，1：表示已经付款）', '例：2018-01-01（没有付款这里为空）', '默认为空', '例：喜歌实业重要通知群(多个群用英文逗号分开)', '默认不同步，1:同步'];
         $data[] = ['大爱名称', '能推送的群'];
         for ($i = 0; $i < count(max($rule, $flockName)); $i++) {
             $data[] = [
@@ -271,7 +300,7 @@ class ExcelController extends Controller
             });
             $excel->sheet('主表', function ($sheet) use ($cellData) {
                 $sheet->rows($cellData);
-                $sheet->cells('A1:K1', function ($cells) {
+                $sheet->cells('A1:L1', function ($cells) {
                     $cells->setAlignment('center');
                     $cells->setBackground('#D2E9FF');
                 });
