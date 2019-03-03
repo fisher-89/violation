d77314d8722d8315ef7b6b59347fc0972db8f517
354226867 <354226867@qq.com>
2019-03-03 15:40:03 +0800

    first bug验证修复

--- routes/admin.php
+++ routes/admin.php
@@ -18,21 +18,22 @@ Route::options('{a?}/{b?}/{c?}', function () {
 });
 Route::group(['middleware' => 'auth:api'], function (RouteContract $admin) {
     $admin->group(['prefix' => 'punish'], function (RouteContract $admin) {
-        $admin->get("/", Controllers\PunishController::class . "@punishList");//列表
-        $admin->get("/{id}", Controllers\PunishController::class . '@getPunishFirst');//变价
-        $admin->post("", Controllers\PunishController::class . "@store");//添加
-        $admin->put("/{id}", Controllers\PunishController::class . '@editPunish');
-        $admin->delete("/{id}", Controllers\PunishController::class . "@delete");//删除
+        $admin->get('/', Controllers\PunishController::class . '@punishList');//列表
+        $admin->get('/{id}', Controllers\PunishController::class . '@getPunishFirst');//变价
+        $admin->post('', Controllers\PunishController::class . '@store');//添加
+        $admin->put('/{id}', Controllers\PunishController::class . '@editPunish');
+        $admin->delete('/{id}', Controllers\PunishController::class . '@delete');//删除
+        $admin->post('batch',Controllers\PunishController::class . '@batchStore');//批量添加
 
-        $admin->post("/pay", Controllers\PunishController::class . "@listPaymentMoney");//展示页面用单向更新已支付
-        $admin->get("/both-pay/{id}", Controllers\PunishController::class . "@detailedPagePayment");//详细页面用双向改变支付状态
+        $admin->post('/pay', Controllers\PunishController::class . '@listPaymentMoney');//展示页面用单向更新已支付
+        $admin->get('/both-pay/{id}', Controllers\PunishController::class . '@detailedPagePayment');//详细页面用双向改变支付状态
 
-        $admin->post("/money", Controllers\CountController::class . "@money");//金额
-        $admin->post("/score", Controllers\CountController::class . "@score");//分值
+        $admin->post('/money', Controllers\CountController::class . '@money');//金额
+        $admin->post('/score', Controllers\CountController::class . '@score');//分值
 
-        $admin->get("/export", Controllers\ExcelController::class . "@export");//Excel导出
-        $admin->post("/import", Controllers\ExcelController::class . "@import");//Excel导入
-        $admin->get("/example", Controllers\ExcelController::class . "@example");//导入模板
+        $admin->get('/export', Controllers\ExcelController::class . '@export');//Excel导出
+        $admin->post('/import', Controllers\ExcelController::class . '@import');//Excel导入
+        $admin->get('/example', Controllers\ExcelController::class . '@example');//导入模板
     });
     $admin->group(['prefix' => 'push'], function (RouteContract $admin) {
         $admin->get('/auth', Controllers\ImageController::class . '@pushingAuthList');//当前用户能推送的群
@@ -42,19 +43,19 @@ Route::group(['middleware' => 'auth:api'], function (RouteContract $admin) {
         $admin->get('/my-log', Controllers\ImageController::class . '@myPushingLog');//我的推送记录
     });
     $admin->group(['prefix' => 'rule'], function (RouteContract $admin) {
-        $admin->get("", Controllers\RuleController::class . "@getList");  //制度表查询
-        $admin->post("", Controllers\RuleController::class . "@store");  //制度表增加
-        $admin->put("/{id}", Controllers\RuleController::class . "@edit");  //制度表修改
-        $admin->delete("/{id}", Controllers\RuleController::class . "@delete");  //制度表删除
-        $admin->get("/{id}", Controllers\RuleController::class . "@getFirst");  //制度表单条详细
-        $admin->get("/math", Controllers\RuleController::class . "@configuration");//拿取公式数据
-        $admin->get("/operator", Controllers\RuleController::class . "@calculations");//拿取运算符
+        $admin->get('', Controllers\RuleController::class . '@getList');  //制度表查询
+        $admin->post('', Controllers\RuleController::class . '@store');  //制度表增加
+        $admin->put('/{id}', Controllers\RuleController::class . '@edit');  //制度表修改
+        $admin->delete('/{id}', Controllers\RuleController::class . '@delete');  //制度表删除
+        $admin->get('/{id}', Controllers\RuleController::class . '@getFirst');  //制度表单条详细
+        $admin->get('/math', Controllers\RuleController::class . '@configuration');//拿取公式数据
+        $admin->get('/operator', Controllers\RuleController::class . '@calculations');//拿取运算符
     });
     $admin->group(['prefix' => 'rule-type'], function (RouteContract $admin) {
-        $admin->get("", Controllers\RuleController::class . "@getTypeList");  //制度分类表查询
-        $admin->post("", Controllers\RuleController::class . "@storeType");  //制度分类表增加
-        $admin->put("/{id}", Controllers\RuleController::class . "@editType");  //制度分类表修改
-        $admin->delete("/{id}", Controllers\RuleController::class . "@delType");  //制度分类表删除
+        $admin->get('', Controllers\RuleController::class . '@getTypeList');  //制度分类表查询
+        $admin->post('', Controllers\RuleController::class . '@storeType');  //制度分类表增加
+        $admin->put('/{id}', Controllers\RuleController::class . '@editType');  //制度分类表修改
+        $admin->delete('/{id}', Controllers\RuleController::class . '@delType');  //制度分类表删除
     });
     $admin->get('count-excel', Controllers\ExcelController::class . '@countStaffExcel');//部门导出
     $admin->get('count-staff-excel',Controllers\ExcelController::class . '@staffExcel');//点进去内人员详细导出
@@ -65,9 +66,9 @@ Route::group(['middleware' => 'auth:api'], function (RouteContract $admin) {
     $admin->get('ding-group',Controllers\ExcelController::class . '@getDingGroup');//获取所有群组  增加推送权限用
 
     $admin->group(['prefix' => 'pushing-auth'], function (RouteContract $admin) {
-        $admin->get("", Controllers\PushAuthController::class . '@index');
-        $admin->post("", Controllers\PushAuthController::class . '@start');
-        $admin->put("/{id}", Controllers\PushAuthController::class . '@edit');
-        $admin->delete("/{id}", Controllers\PushAuthController::class . '@delete');
+        $admin->get('', Controllers\PushAuthController::class . '@index');
+        $admin->post('', Controllers\PushAuthController::class . '@start');
+        $admin->put('/{id}', Controllers\PushAuthController::class . '@edit');
+        $admin->delete('/{id}', Controllers\PushAuthController::class . '@delete');
     });
 });
