babb926d4d03681cfc127f60822add49a974387b
354226867 <354226867@qq.com>
2019-03-06 14:24:26 +0800

    first bug验证修复

--- app/Models/PushingAuthority.php
+++ app/Models/PushingAuthority.php
@@ -8,8 +8,6 @@ class PushingAuthority extends Model
 {
     protected $table = 'pushing_authority';
 
-    protected $timestamps = false;
-
     protected $fillable = [
         'staff_sn', 'staff_name', 'flock_name', 'flock_sn', 'default_push'
     ];
